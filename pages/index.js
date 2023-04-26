e943ddd7be4ada79a091d1b890d179ee4626cfa0
axel <axosar@gmail.com>
2023-04-25 20:34:39 -0600

    user-data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Home() {
+  const {user} = useUser();
+
+  console.log('USER: ', user);
+
   return (
     <div>
       <div>
-        <Link href="/api/auth/login">LOGIN</Link>
+        {!!user ? (
+          <>
+            <div>
+              <Image src={user.picture} alt={user.name} width={50} height={50} />
+            </div>
+            <Link href="/api/auth/logout">LOGOUT</Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">LOGIN</Link>
+        )}
+        
       </div>
     </div>
   );
