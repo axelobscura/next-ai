44bb332a6604b8a360d2ea40a44ab0d7201bb704
axel <axosar@gmail.com>
2023-04-26 19:57:21 -0600

    protected-routes

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,16 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function NewPost(props) {
+  console.log(props);
   return (
     <div>
       <h1>New Post</h1>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired (() => {
+  return {
+    props: {}
+  }
+});
