80523c25f2979619fd37c38fffe50a198b8214e1
axel <axosar@gmail.com>
2023-04-26 20:22:52 -0600

    sidebar-styling

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,8 +1,22 @@
 export const AppLayout = ({children}) => {
   return(
-    <div>
-      This is the app layout
-      {children}
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800">
+          <div>LOGO</div>
+          <div>CTA</div>
+          <div>TOKENS</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          LIST OF POSTS
+        </div>
+        <div className="bg-cyan-800">
+          USER INFORMATION
+        </div>
+      </div>
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
